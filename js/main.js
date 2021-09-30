f7f31080b26562650e226ea2a4958ea3a46daa77
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2021-09-30 18:15:20 -0300

    terminando el menú mobile

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
+
 // Abrir Menu
 const menu = document.getElementById('burgerMenu');
 if(menu){
-menu.addEventListener("click", () => {
-    document.querySelector('.menu_screen').classList.add('active');
-    console.log('click')
-})
+    menu.addEventListener("click", () => {
+        document.querySelector('.menu_screen').classList.add('active');
+        console.log('click')
+    })
 }
 
 // Cerrar Menu
 const close = document.querySelector('.close');
 if(close){
-close.addEventListener("click", () => {
-    document.querySelector('.menu_screen').classList.remove('active');
-    console.log('click')
-})
+    close.addEventListener("click", () => {
+        document.querySelector('.menu_screen').classList.remove('active');
+    })
 }
 
 // Cerrar el menu cuando se le da click a un link del menu
 let links = document.querySelectorAll('.menu_screen a');
 
 links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        document.querySelector('.menu_screen').classList.remove('active');
+    link.addEventListener('click', function() {
+        document.querySelector('.menu_screen').classList.remove('active');        
     })
 })
